60dd03b34462e50e1c745108235ba8c5a4ab833f
Nicolas Olmos <yohannicolas2003@gmail.com>
2022-05-22 14:35:35 -0500

    Create MVC folders and Terminal class into view folder

---
+++ src/view/Terminal.java
@@ -0,0 +1,53 @@
+package view;
+
+import java.util.Scanner;
+
+public class Terminal {
+    private String title;
+    private Scanner sc;
+
+    public Terminal(String title, Scanner sc) {
+        this.title = title;
+        this.sc = sc;
+    }
+
+    public Terminal() {
+        this.title = "";
+        this.sc = new Scanner(System.in);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Scanner getSc() {
+        return sc;
+    }
+
+    public void setSc(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public int inputInteger(String msg) {
+        System.out.print(msg);
+        return sc.nextInt();
+    }
+
+    public double inputDouble(String msg) {
+        System.out.print(msg);
+        return sc.nextDouble();
+    }
+
+    public String inputString(String msg) {
+        System.out.print(msg);
+        return sc.nextLine();
+    }
+
+    public void print(String msg) {
+        System.out.print(msg);
+    }
+}
