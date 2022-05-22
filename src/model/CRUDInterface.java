004f428dc7cc3aad29f3f33eb9df9f2b665e5c02
Nicolas Olmos <yohannicolas2003@gmail.com>
2022-05-22 17:14:26 -0500

    Add add method into CRUDInterface

--- src/model/CRUDInterface.java
+++ src/model/CRUDInterface.java
@@ -1,6 +1,7 @@
 package model;
 
 public interface CRUDInterface {
+    public void add();
     public String read();
     public void updateById(String id, Component component);
     public void deleteById(String id, Component component);
