b2d983f4766437be1c9594432e7465246183bccb
Nicolas Olmos <yohannicolas2003@gmail.com>
2022-05-22 15:04:22 -0500

    Delete useless method

--- src/model/CRUDInterface.java
+++ src/model/CRUDInterface.java
@@ -1,7 +1,6 @@
 package model;
 
 public interface CRUDInterface {
-    public void create();
     public void read();
     public void updateById(String id);
     public void deleteById(String id);
